05c4628e41f9de9ac07319ad2948797c42925d6d
Gourav Malviya <gouravmalviya12@gmail.com>
2021-10-17 16:26:35 +0530

    added more code

---
+++ src/main/java/com/omnicell/recipe/RecipeOmnicellApplication.java
@@ -0,0 +1,13 @@
+package com.omnicell.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeOmnicellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeOmnicellApplication.class, args);
+	}
+
+}

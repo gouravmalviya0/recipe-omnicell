7fd149a45649289cd4031bd75157143eac088d1f
Gourav Malviya <gouravmalviya12@gmail.com>
2021-10-17 17:15:04 +0530

    added method to check status

--- src/main/java/com/omnicell/recipe/controller/RecipeController.java
+++ src/main/java/com/omnicell/recipe/controller/RecipeController.java
@@ -1,8 +1,15 @@
 package com.omnicell.recipe.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RecipeController {
+	
+	@GetMapping("/status")
+	public String status() {
+		return "UP";
+		
+	}
 
 }

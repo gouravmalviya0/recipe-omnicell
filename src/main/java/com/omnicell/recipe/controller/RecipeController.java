1ddce56fe9742015ebb5da9e511543d3550f0e83
Gourav Malviya <gouravmalviya12@gmail.com>
2021-10-17 16:19:24 +0530

    added basic code

---
+++ src/main/java/com/omnicell/recipe/controller/RecipeController.java
@@ -0,0 +1,8 @@
+package com.omnicell.recipe.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecipeController {
+
+}
---
+++ src/main/java/com/omnicell/recipe/service/RecipeService.java
@@ -0,0 +1,5 @@
+package com.omnicell.recipe.service;
+
+public interface RecipeService {
+
+}
